27b36fa00be36b95aeaf24391406c1c6ec048470
Bibix93 <baraa.elalami@gmail.com>
2021-09-13 13:04:52 +0100

    Part 2

--- hashing/hashing.js
+++ hashing/hashing.js
@@ -28,16 +28,71 @@ Blockchain.blocks.push({
 });
 
 // TODO: insert each line into blockchain
-// for (let line of poem) {
-// }
+for (let line of poem) {
+	
+	createBlock(line);
+	
+};
 
-// console.log(`Blockchain is valid: ${verifyChain(Blockchain)}`);
+function createBlock(_data){
+	
+	let prevHash = blockHash(_data);
+	let block = {
+		index: Blockchain.blocks.length ,
+		prevHash: Blockchain.blocks[Blockchain.blocks.length-1].hash,
+		data: _data,
+		timestamp: Date.now(),
+	}
+	block.hash = blockHash(block);
+	Blockchain.blocks.push(block);
+	console.log(block);
+	return block;
+
+};
+
+console.log(`Blockchain is valid: ${verifyChain(Blockchain)}`);
 
 
 // **********************************
 
 function blockHash(bl) {
+	// let block = JSON.stringify(bl)
 	return crypto.createHash("sha256").update(
 		// TODO: use block data to calculate hash
+		`${bl.index};${bl.prevHash};${bl.data};${bl.timestamp};`
+		// block
 	).digest("hex");
-}
+};
+
+function verifyBlock(block) {
+	let verify = true;
+
+	if(block.data==="" || block.prevHash==="" || !Number.isInteger(block.index) || block.index<0) {
+		verify = false;
+	}
+	if(block.hash!= blockHash(block)) {
+		verify = false;
+	}
+
+
+	return verify;
+
+};
+
+
+function verifyChain(chain) {
+	let verify = true;
+	if(chain.blocks[0].hash!="000000") {
+		verify = false;
+	}
+	let i =1;
+	while( i< chain.blocks.length) {
+		verify = verify && verifyBlock(chain.blocks[i]);
+		i++;
+
+	}
+	return verify;
+};
+
+
+
